9a747e114fadb72fc900ec08f21c490e161b7484
Johnny Bergström <johnny.bergstrom@duego.com>
2012-12-12 15:53:33 +0100

    Add port option

--- tail.js
+++ tail.js
@@ -5,17 +5,21 @@ var app = require('http').createServer(handler),
 	fs = require('fs'),
 	sys = require('sys'),
 	spawn = require('child_process').spawn,
-	filename = process.argv[2];
+	filename = process.argv[2],
+	port = process.argv[3];
 
-if (! filename) {
-	console.error("Usage <node> <filename>");
+if ((process.argv.length < 3) || (process.argv.length > 4)) {
+	console.error("Usage <node> <filename> [port]");
 	process.exit(1);
+}
+if (! port) {
+	port = 80;
 }
 var tail = spawn("tail", ["-f", filename]);
 tail.stdout.setEncoding('utf8');
 console.log("Tailing " + filename);
 
-app.listen(80);
+app.listen(port);
 function handler(req, res) {
 	fs.readFile(__dirname + '/tail.html', function(err, data) {
 		if (err) {
